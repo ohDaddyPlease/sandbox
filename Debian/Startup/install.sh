b8e95753cdb4fcf3550a2d4cd058b15b76754597
Sergius Novikov <kraspvp@mail.ru>
2024-03-06 02:34:04 +0300

    Update install.sh

--- Debian/Startup/install.sh
+++ Debian/Startup/install.sh
@@ -1,4 +1,4 @@
 su - -c "groupadd docker || true && usermod -aG docker,sudo,adm $USER && \
 apt update && \
-apt install -y vim git openssl gcc automake libtool net-tools autoconf cmake jq tmux wget perl lua5.4 && \
+apt install -y vim git openssl gcc automake libtool net-tools autoconf cmake jq tmux wget perl lua5.4 lsb-release curl gpg && \
 reboot"
