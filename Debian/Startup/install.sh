1cc7db1741b4efe3c07a1dffdb061d7275b44b43
Sergius Novikov <kraspvp@mail.ru>
2024-03-06 01:38:55 +0300

    Create install.sh

---
+++ Debian/Startup/install.sh
@@ -0,0 +1,4 @@
+su - -c "groupadd docker || true && usermod -aG docker,sudo $USER && \
+apt update && \
+apt install -y vim git openssl gcc automake libtool net-tools autoconf cmake jq tmux wget perl lua5.4 && \
+reboot"
