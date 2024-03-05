1380d9f8164d6ba29513d55bb974ce5ce8ec5b66
Sergius Novikov <kraspvp@mail.ru>
2024-03-05 23:17:23 +0300

    Create install.sh

---
+++ Debian/bare-metal/Golang/install.sh
@@ -0,0 +1,6 @@
+wget https://go.dev/dl/go1.22.0.linux-arm64.tar.gz && \
+sudo rm -rf /usr/local/go && \
+sudo tar -C /usr/local -xzf go1.22.0.linux-arm64.tar.gz && \
+echo -e "\nexport PATH=\$PATH:/usr/local/go/bin" > $HOME/.bash_profile && \
+source ~/.bash_profile && \
+go version
