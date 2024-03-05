e226c80f3179e283f44590b5cca91ba1031c2776
Sergius Novikov <kraspvp@mail.ru>
2024-03-06 01:15:10 +0300

    Update install.sh

--- Debian/bare-metal/Golang/install.sh
+++ Debian/bare-metal/Golang/install.sh
@@ -1,6 +1,8 @@
-wget https://go.dev/dl/go1.22.0.linux-$(dpkg --print-architecture).tar.gz && \
+archive="go1.22.0.linux-$(dpkg --print-architecture).tar.gz" && \
+sudo rm -f $archive && \
+wget https://go.dev/dl/$archive && \
 sudo rm -rf /usr/local/go && \
-sudo tar -C /usr/local -xzf go1.22.0.linux-$(dpkg --print-architecture).tar.gz && \
+sudo tar -C /usr/local -xzf $archive && \
 echo -e "\nexport PATH=\$PATH:/usr/local/go/bin" > $HOME/.bash_profile && \
 source ~/.bash_profile && \
 go version
